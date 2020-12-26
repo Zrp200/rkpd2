c95d55b0c4b81cb63caa658703b30d629edc533c
Zrp200 <zrp200@gmail.com>
2020-12-25 22:55:11 -0800

    Added Explosive enchantment, is not accessible yet

---
+++ core/src/main/java/com/zrp200/rkpd2/items/weapon/enchantments/Explosive.java
@@ -0,0 +1,24 @@
+package com.zrp200.rkpd2.items.weapon.enchantments;
+
+import com.watabou.utils.Random;
+import com.zrp200.rkpd2.actors.Char;
+import com.zrp200.rkpd2.items.bombs.Bomb;
+import com.zrp200.rkpd2.items.weapon.Weapon;
+import com.zrp200.rkpd2.sprites.ItemSprite;
+
+// huntress bow exclusive enchant. I'm sure everyone's going to love this one.
+public class Explosive extends Weapon.Enchantment {
+    // placeholder color
+    private static final ItemSprite.Glowing REDORANGE = new ItemSprite.Glowing( 0xFF5349 );
+    @Override
+    public ItemSprite.Glowing glowing() {
+        return REDORANGE;
+    }
+
+    @Override
+    public int proc(Weapon weapon, Char attacker, Char defender, int damage) {
+        int level = weapon.buffedLvl();
+        if(Random.Int(4+level) >= 3) new Bomb().explode(defender.pos);
+        return damage;
+    }
+}
