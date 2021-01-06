3fac2f00133b5a7f40a78d60cb46442a5f89b728
Zrp200 <zrp200@gmail.com>
2021-01-05 17:33:47 -0800

    Revert "Implemented a buffed Mystical Upgrade. Now recharges an extra charge."
    This reverts commit 1af543b8

--- core/src/main/java/com/zrp200/rkpd2/actors/hero/Talent.java
+++ core/src/main/java/com/zrp200/rkpd2/actors/hero/Talent.java
@@ -362,9 +362,7 @@ public enum Talent {
 		if (hero.hasTalent(MYSTICAL_UPGRADE,RESTORATION)){
 			CloakOfShadows cloak = hero.belongings.getItem(CloakOfShadows.class);
 			if (cloak != null){
-				int recharge = hero.pointsInTalents(MYSTICAL_UPGRADE,RESTORATION);
-				if(hero.hasTalent(MYSTICAL_UPGRADE)) recharge++;
-				cloak.overCharge(recharge);
+				cloak.overCharge(hero.pointsInTalents(MYSTICAL_UPGRADE,RESTORATION));
 				ScrollOfRecharging.charge( Dungeon.hero );
 				SpellSprite.show( hero, SpellSprite.CHARGE );
 			}
