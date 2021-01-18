70cd75a8b1857e27025e285192de7a42737d1673
Zrp200 <zrp200@gmail.com>
2021-01-18 13:18:58 -0800

    Restored old DM-300 spawn logic, now starts directly after player moves.

--- core/src/main/java/com/zrp200/rkpd2/levels/NewCavesBossLevel.java
+++ core/src/main/java/com/zrp200/rkpd2/levels/NewCavesBossLevel.java
@@ -229,15 +229,12 @@ public class NewCavesBossLevel extends Level {
 	public void occupyCell(Char ch) {
 		super.occupyCell(ch);
 
-		//seal the level when the hero moves near to a pylon, the level isn't already sealed, and the gate hasn't been destroyed
+		//seal the level when the hero moves off the entrance, the level isn't already sealed, and the gate hasn't been destroyed
 		int gatePos = pointToCell(new Point(gate.left, gate.top));
-		if (ch == Dungeon.hero && !locked && solid[gatePos]){
-			for (int pos : pylonPositions){
-				if (Dungeon.level.distance(ch.pos, pos) <= 3){
-					seal();
-					break;
-				}
-			}
+		if (ch == Dungeon.hero && ch.pos != entrance && !locked && solid[gatePos]){
+
+			seal();
+
 		}
 	}
 
