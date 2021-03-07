9a36cc828e70150bfbbd107058b2e5679a747f43
Zrp200 <zrp200@gmail.com>
2021-03-07 11:44:42 -0800

    Reworked Rat King's armor again.
    * now tracks the individual stages; jump/molten/blades
    * each stage applies its delay separately at the end
    * adjusted heroic leap portion, was missing a `curUser.move()`, which was part of its sfx. also moved its vfx after the Dungeon.observe() call so as to be as consistent as possible with the standard heroic leap.
    * the blades delay is now equal to the hero's attack delay, which means it is cancelled if lethal momentum is active.
    * if no enemies are in sight at the end, spectral blades and molten earth are entirely skipped (saying no enemies in sight), including delay.
    * selecting yourself will allow you to skip to jump part of the move.

--- core/src/main/java/com/zrp200/rkpd2/items/armor/RatKingArmor.java
+++ core/src/main/java/com/zrp200/rkpd2/items/armor/RatKingArmor.java
@@ -52,61 +52,69 @@ public class RatKingArmor extends ClassArmor{
         public void onSelect( Integer target ) {
             if(target == null) return;
 
-            PathFinder.buildDistanceMap(curUser.pos, BArray.not(Dungeon.level.solid, null), 8);
+            boolean[] stages = new boolean[3]; // jump/molten/blades
 
-            if (PathFinder.distance[target] == Integer.MAX_VALUE ||
-                    !Dungeon.level.heroFOV[target]) {
-                GLog.w(Messages.get(RogueArmor.class, "fov"));
-                return;
-            }
-            if(Actor.findChar(target) != null) { // use heroic leap mechanics instead.
-                Ballistica route = new Ballistica(curUser.pos, target, Ballistica.STOP_TARGET);
-                //can't occupy the same cell as another char, so move back one until it is valid.
-                int i = 0;
-                while (Actor.findChar( target ) != null && target != curUser.pos)
-                    target = route.path.get(route.dist-++i);
-            }
+            stages[0] = target != curUser.pos;
+            if(stages[0]) {
+                PathFinder.buildDistanceMap(curUser.pos, BArray.not(Dungeon.level.solid, null), 8);
 
-            charge -= 35;
-            updateQuickslot();
-            for (Mob mob : Dungeon.level.mobs.toArray(new Mob[0])) {
-                if (Dungeon.level.adjacent(mob.pos, curUser.pos) && mob.alignment != Char.Alignment.ALLY) {
-                    Buff.prolong(mob, Blindness.class, Blindness.DURATION / 2f);
-                    if (mob.state == mob.HUNTING) mob.state = mob.WANDERING;
-                    mob.sprite.emitter().burst(Speck.factory(Speck.LIGHT), 4);
+                if (PathFinder.distance[target] == Integer.MAX_VALUE ||
+                        !Dungeon.level.heroFOV[target]) {
+                    GLog.w(Messages.get(RogueArmor.class, "fov"));
+                    return;
                 }
+
+                if (Actor.findChar(target) != null) { // use heroic leap mechanics instead.
+                    Ballistica route = new Ballistica(curUser.pos, target, Ballistica.STOP_TARGET);
+                    //can't occupy the same cell as another char, so move back one until it is valid.
+                    int i = 0;
+                    while (Actor.findChar(target) != null && target != curUser.pos)
+                        target = route.path.get(route.dist - ++i);
+                }
+
+                for (Mob mob : Dungeon.level.mobs.toArray(new Mob[0])) {
+                    if (Dungeon.level.adjacent(mob.pos, curUser.pos) && mob.alignment != Char.Alignment.ALLY) {
+                        Buff.prolong(mob, Blindness.class, Blindness.DURATION / 2f);
+                        if (mob.state == mob.HUNTING) mob.state = mob.WANDERING;
+                        mob.sprite.emitter().burst(Speck.factory(Speck.LIGHT), 4);
+                    }
+                }
+                Buff.prolong(curUser, Invisibility.class, Invisibility.DURATION / 2f);
+                CellEmitter.get(curUser.pos).burst(Speck.factory(Speck.WOOL), 10);
+                ScrollOfTeleportation.appear(curUser, target);
+                curUser.move(target); // impact from warrior leap.
+                Sample.INSTANCE.play(Assets.Sounds.PUFF);
+                Dungeon.level.occupyCell(curUser);
+                // at least do warrior vfx now.
+                Dungeon.observe();
+                GameScene.updateFog();
+                CellEmitter.center(curUser.pos).burst(Speck.factory(Speck.DUST), 10);
+                Camera.main.shake(2, 0.5f);
             }
-            Buff.prolong(curUser, Invisibility.class, Invisibility.DURATION/2f);
-            CellEmitter.get(curUser.pos).burst(Speck.factory(Speck.WOOL), 10);
-            ScrollOfTeleportation.appear(curUser, target);
-            Sample.INSTANCE.play(Assets.Sounds.PUFF);
-            Dungeon.level.occupyCell(curUser);
-            // at least do warrior vfx now.
-            CellEmitter.center(curUser.pos).burst(Speck.factory(Speck.DUST), 10);
-            Camera.main.shake(2, 0.5f);
-            Dungeon.observe();
-            GameScene.updateFog();
             // now do mage
             for (Mob mob : Dungeon.level.mobs.toArray(new Mob[0])) {
                 if (Dungeon.level.heroFOV[mob.pos]
                         && mob.alignment != Char.Alignment.ALLY) {
+                    stages[2] = true;
                     Buff.affect( mob, Burning.class ).reignite( mob );
                     Buff.prolong( mob, Roots.class, Roots.DURATION );
                     mob.damage(Random.NormalIntRange(4, 16 + Dungeon.depth), new Burning());
                 }
             }
 
-            Dungeon.observe();
-            curUser.sprite.operate( curUser.pos );
-            //Invisibility.dispel();
-            curUser.busy();
+            if(stages[2]) {
+                Dungeon.observe();
+                curUser.sprite.operate(curUser.pos);
+                //Invisibility.dispel();
+                curUser.busy();
 
-            curUser.sprite.emitter().start( ElmoParticle.FACTORY, 0.025f, 20 );
-            Sample.INSTANCE.play( Assets.Sounds.BURNING );
-            Sample.INSTANCE.play( Assets.Sounds.BURNING );
-            Sample.INSTANCE.play( Assets.Sounds.BURNING );
-            // warrior
-            for (int i = 0; i < PathFinder.NEIGHBOURS8.length; i++) {
+                curUser.sprite.emitter().start(ElmoParticle.FACTORY, 0.025f, 20);
+                Sample.INSTANCE.play(Assets.Sounds.BURNING);
+                Sample.INSTANCE.play(Assets.Sounds.BURNING);
+                Sample.INSTANCE.play(Assets.Sounds.BURNING);
+            }
+            // warrior. this is delayed so the burst burning damage doesn't cancel this out instantly.
+            if(stages[0]) for (int i = 0; i < PathFinder.NEIGHBOURS8.length; i++) {
                 Char mob = Actor.findChar(curUser.pos + PathFinder.NEIGHBOURS8[i]);
                 if (mob != null && mob != curUser && mob.alignment != Char.Alignment.ALLY) {
                     Buff.prolong(mob, Paralysis.class, 5);
@@ -120,6 +128,7 @@ public class RatKingArmor extends ClassArmor{
                 if (Dungeon.level.distance(curUser.pos, mob.pos) <= 12
                         && Dungeon.level.heroFOV[mob.pos]
                         && mob.alignment != Char.Alignment.ALLY) {
+                    stages[2] = true;
                     Callback callback = new Callback() {
                         @Override
                         public void call() {
@@ -127,16 +136,22 @@ public class RatKingArmor extends ClassArmor{
                             Invisibility.dispel();
                             targets.remove( this );
                             if (targets.isEmpty()) {
-                                //Buff.prolong(curUser, Invisibility.class, Invisibility.DURATION/2f);
-                                Actor.addDelayed(new Actor() {
-                                    @Override
-                                    protected boolean act() {
-                                        Buff.prolong(curUser, Invisibility.class, Invisibility.DURATION/2f);
-                                        remove(this);
-                                        return true;
-                                    }
-                                }, 2f);
-                                curUser.spendAndNext(3);
+                                int delay = 0;
+                                if(stages[1]) delay++;
+                                if(stages[2]) delay += curUser.attackDelay();
+                                if(stages[0]) Actor.addDelayed(new Actor() {
+                                        @Override
+                                        protected boolean act() {
+                                            Buff.prolong(curUser, Invisibility.class, Invisibility.DURATION/2f);
+                                            remove(this);
+                                            return true;
+                                        }
+                                    }, delay++);
+                                curUser.spendAndNext(delay);
+                                if(stages[0] || stages[1] || stages[2]) {
+                                    charge -= 35;
+                                    updateQuickslot();
+                                }
                             }
                         }
                     };
@@ -151,7 +166,15 @@ public class RatKingArmor extends ClassArmor{
                 curUser.sprite.zap(curUser.pos);
                 curUser.busy();
             }
-            //curUser.spendAndNext(Actor.TICK*3 ); // punishment
+            // edge case where's there no enemies.. skips all the delay but then again...
+            if(!stages[1] && !stages[2]) {
+                GLog.w( Messages.get(HuntressArmor.class, "no_enemies") );
+                if(stages[0]) {
+                    curUser.spendAndNext(1);
+                    charge -= 35;
+                    updateQuickslot();
+                }
+            }
         }
         @Override
         public String prompt() {
