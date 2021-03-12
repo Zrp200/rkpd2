13de2cce79d6d9838badf1bd5e1a31b244fcb2d1
Zrp200 <zrp200@gmail.com>
2021-03-12 13:09:17 -0800

    Reverted the change to rat king's access to enhanced rings and light cloak from cf6a42a2
    * this just made it more confusing and awkward to work with more than anything else.

--- core/src/main/java/com/zrp200/rkpd2/actors/hero/Talent.java
+++ core/src/main/java/com/zrp200/rkpd2/actors/hero/Talent.java
@@ -371,7 +371,7 @@ public enum Talent {
 
 	public static void onArtifactUsed( Hero hero ){
 		if (hero.hasTalent(ENHANCED_RINGS,RK_FREERUNNER)){
-			Buff.prolong(hero, EnhancedRings.class, 3f*hero.pointsInTalent(ENHANCED_RINGS,RK_FREERUNNER));
+			Buff.prolong(hero, EnhancedRings.class, 3f*hero.pointsInTalent(ENHANCED_RINGS,RK_ASSASSIN));
 		}
 	}
 	public static void onItemEquipped( Hero hero, Item item ){
