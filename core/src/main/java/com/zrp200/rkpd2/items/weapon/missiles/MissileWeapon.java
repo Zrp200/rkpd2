62b6388c3793d94090397ba4757cee1c8ec6b43e
Zrp200 <zrp200@gmail.com>
2021-03-25 10:07:28 -0700

    Removed enchanted crossbow limitation from shared enchantment, and adjusted wording of some text.

--- core/src/main/java/com/zrp200/rkpd2/items/weapon/missiles/MissileWeapon.java
+++ core/src/main/java/com/zrp200/rkpd2/items/weapon/missiles/MissileWeapon.java
@@ -154,7 +154,7 @@ abstract public class MissileWeapon extends Weapon {
 
 		boolean projecting = hasEnchant(Projecting.class, user);
 		if (!projecting && Random.Int(3) < user.pointsInTalent(Talent.SHARED_ENCHANTMENT,Talent.RK_SNIPER)){
-			if (this instanceof Dart && ((Dart) this).crossbowHasEnchant(Dungeon.hero)){
+			if (this instanceof Dart && ((Dart) this).crossbowHasEnchant(Dungeon.hero) && !user.hasTalent(Talent.SHARED_ENCHANTMENT)){ // how DARE evan crush huntress synergies???
 				//do nothing
 			} else {
 				SpiritBow bow = Dungeon.hero.belongings.getItem(SpiritBow.class);
@@ -202,8 +202,8 @@ abstract public class MissileWeapon extends Weapon {
 	@Override
 	public int proc(Char attacker, Char defender, int damage) {
 		if (attacker == Dungeon.hero && Random.Int(3) < Dungeon.hero.pointsInTalent(Talent.SHARED_ENCHANTMENT,Talent.RK_SNIPER)){
-			if (this instanceof Dart && ((Dart) this).crossbowHasEnchant(Dungeon.hero)){
-				//do nothing
+			if (this instanceof Dart && ((Dart) this).crossbowHasEnchant(Dungeon.hero) && !Dungeon.hero.hasTalent(Talent.SHARED_ENCHANTMENT)){ // HUNTRESS MUST BE BUFFED
+ 				//do nothing
 			} else {
 				SpiritBow bow = Dungeon.hero.belongings.getItem(SpiritBow.class);
 				if (bow != null && bow.enchantment != null && Dungeon.hero.buff(MagicImmune.class) == null) {
