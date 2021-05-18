cd3e97ef8b1c6afcf5ef22d8b39eaf641f6d7bbc
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-05-18 18:05:17 +0300

    ouch

--- core/src/main/java/com/zrp200/rkpd2/Dungeon.java
+++ core/src/main/java/com/zrp200/rkpd2/Dungeon.java
@@ -321,7 +321,7 @@ public class Dungeon {
 	}
 	
 	public static boolean shopOnLevel() {
-		return depth == 6 || depth == 11 || depth == 16;
+		return depth == 6 || depth == 11 || depth == 16 || (depth - 1 % 5 == 0);
 	}
 	
 	public static boolean bossLevel() {
