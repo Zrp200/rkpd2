4d1597ea003a2481acc0a798f8664a179d18ae70
Zrp200 <zrp200@gmail.com>
2021-06-15 12:04:35 -0700

    Cleave now is shifted

--- core/src/main/java/com/zrp200/rkpd2/actors/buffs/Combo.java
+++ core/src/main/java/com/zrp200/rkpd2/actors/buffs/Combo.java
@@ -97,8 +97,7 @@ public class Combo extends Buff implements ActionIndicator.Action {
 
 		if (!enemy.isAlive() || (enemy.buff(Corruption.class) != null && enemy.HP == enemy.HT)){
 			Hero hero = (Hero)target;
-			int time = 15 * hero.pointsInTalent(Talent.CLEAVE,Talent.RK_GLADIATOR);
-			if(Dungeon.hero.hasTalent(Talent.CLEAVE)) time *= 2;
+			int time = 15 * hero.shiftedPoints(Talent.CLEAVE,Talent.RK_GLADIATOR);
 			comboTime = Math.max(comboTime, time);
 		}
 		incCombo();
