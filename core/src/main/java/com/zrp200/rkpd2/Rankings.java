6998bacd2fab14a311db06c4eb737a3b48379676
Zrp200 <zrp200@gmail.com>
2021-06-14 19:32:07 -0700

    Made !!!NO TEXT FOUND!!! a constant

--- core/src/main/java/com/zrp200/rkpd2/Rankings.java
+++ core/src/main/java/com/zrp200/rkpd2/Rankings.java
@@ -277,7 +277,7 @@ public enum Rankings {
 				return Messages.get(this, "something");
 			} else {
 				String result = Messages.get(cause, "rankings_desc", (Messages.get(cause, "name")));
-				if (result.contains("!!!NO TEXT FOUND!!!")){
+				if (result.contains(Messages.NULL)){
 					return Messages.get(this, "something");
 				} else {
 					return result;
