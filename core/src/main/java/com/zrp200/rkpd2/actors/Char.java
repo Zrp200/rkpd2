997da271264a8363180cdf842360b10b96f15c64
Zrp200 <zrp200@gmail.com>
2021-08-06 11:27:41 -0700

    Char.java: Removed legacy warlock's touch code

--- core/src/main/java/com/zrp200/rkpd2/actors/Char.java
+++ core/src/main/java/com/zrp200/rkpd2/actors/Char.java
@@ -325,16 +325,6 @@ public abstract class Char extends Actor {
 
 			if (this instanceof Hero){
 				Hero hero = (Hero)this;
-				KindOfWeapon wep = hero.belongings.weapon;
-				// fixme should I let allies be able to soul mark?
-				// 20%/40%/60% for this to work.
-				if(Random.Int(5) < hero.pointsInTalent(Talent.WARLOCKS_TOUCH)) {
-					// warlock can soul mark by simply attacking with warlock's touch.
-					int lvl = wep != null ? wep.buffedLvl() : 0;
-					// it's treated as a wand, so apply wand boost if applicable.
-					if(hero.heroClass == HeroClass.MAGE) lvl += HeroClass.MAGE_WAND_BOOST;
-					SoulMark.process(enemy,lvl,1,true);
-				}
 				if (hero.belongings.weapon() instanceof MissileWeapon
 						&& (hero.subClass == HeroSubClass.SNIPER || hero.subClass == HeroSubClass.KING)
 						&& !Dungeon.level.adjacent(hero.pos, enemy.pos)){
