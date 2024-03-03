5c4b734b7a5857c2393773e27d5ab2568b774377
Zachary Perlmutter <zrp200@gmail.com>
2024-03-03 14:20:57 -0800

    fix #2 (rare crash bug with evil eyes)

--- core/src/main/java/com/zrp200/rkpd2/actors/mobs/Eye.java
+++ core/src/main/java/com/zrp200/rkpd2/actors/mobs/Eye.java
@@ -93,7 +93,7 @@ public class Eye extends Mob {
 		if (beamCooldown == 0) {
 			Ballistica aim = new Ballistica(pos, enemy.pos, Ballistica.STOP_SOLID);
 
-			if (enemy.invisible == 0 && !isCharmedBy(enemy) && fieldOfView[enemy.pos]
+			if (enemy.invisible == 0 && !isCharmedBy(enemy) && fieldOfView != null && fieldOfView[enemy.pos]
 					&& (super.canAttack(enemy) || aim.subPath(1, aim.dist).contains(enemy.pos))){
 				if(Actor.current() == this) {
 					beam = aim;
